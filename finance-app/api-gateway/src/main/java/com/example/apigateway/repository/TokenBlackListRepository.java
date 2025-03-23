2390492c1a4889ca28bba168986f3e1413fd8c60
Bharadwaj <kodi_b200289ee@nitc.ac.in>
2025-03-23 14:26:26 +0530

    implemented scheduled to delete expired tokens

--- finance-app/api-gateway/src/main/java/com/example/apigateway/repository/TokenBlackListRepository.java
+++ finance-app/api-gateway/src/main/java/com/example/apigateway/repository/TokenBlackListRepository.java
@@ -3,7 +3,11 @@ package com.example.apigateway.repository;
 import com.example.apigateway.model.BlackListedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface TokenBlackListRepository extends JpaRepository<BlackListedToken, Integer> {
 
     public BlackListedToken findByToken(String token);
+
+    void deleteByExpiryBefore(LocalDateTime now);
 }
--- finance-app/api-gateway/src/main/java/com/example/apigateway/service/TokenBlacklistService.java
+++ finance-app/api-gateway/src/main/java/com/example/apigateway/service/TokenBlacklistService.java
@@ -2,9 +2,12 @@ package com.example.apigateway.service;
 
 import com.example.apigateway.model.BlackListedToken;
 import com.example.apigateway.repository.TokenBlackListRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Service
@@ -22,6 +25,14 @@ public class TokenBlacklistService {
             return false;
         }
         return true;
+    }
+
+    @Scheduled(fixedRate = 3600000) // Runs every 1 hour
+    @Transactional
+    public void removeExpiredTokens() {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Checking for expired tokens at: " + now);
+        tokenBlacklistRepository.deleteByExpiryBefore(now);  // Deletes expired tokens
     }
 }
 
