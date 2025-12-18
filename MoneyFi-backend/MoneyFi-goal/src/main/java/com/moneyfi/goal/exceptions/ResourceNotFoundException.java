773396909d480c2c66004c2c19fc7809bd81fcad
Bharadwaj <kodi_b200289ee@nitc.ac.in>
2025-12-18 23:41:57 +0530

    [Feature] Removing goal service

--- MoneyFi-backend/MoneyFi-goal/src/main/java/com/moneyfi/goal/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.moneyfi.goal.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
