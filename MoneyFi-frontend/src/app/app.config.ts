7243f17e1141d4a8ede87f05d50651a5f15adaa4
Bharadwaj <kodi_b200289ee@nitc.ac.in>
2025-12-27 15:50:11 +0530

    Deplyoing ui using env variables

--- MoneyFi-frontend/src/app/app.config.ts
+++ MoneyFi-frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,12 +7,24 @@ import { authInterceptor } from './auth.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { ToastrModule } from 'ngx-toastr';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { RuntimeEnvService } from './core/config/runtime-env.service';
 
+export function initEnv(envService: RuntimeEnvService) {
+  return () => envService.load();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])), provideAnimationsAsync(), importProvidersFrom(ToastrModule.forRoot()),
     provideNativeDateAdapter(), provideAnimationsAsync(),
+    provideHttpClient(),
+    RuntimeEnvService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initEnv,
+      deps: [RuntimeEnvService],
+      multi: true
+    }
   ],
 };
