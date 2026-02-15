14816c73ff79abe364215e6fa9558a9b920bf7ac
Bharadwaj <kodi_b200289ee@nitc.ac.in>
2026-02-15 16:27:57 +0530

    [Bugfix] Timer api missing resolved

--- MoneyFi-backend/MoneyFi-api-gateway/src/main/java/com/moneyfi/apigateway/controller/common/CommonController.java
+++ MoneyFi-backend/MoneyFi-api-gateway/src/main/java/com/moneyfi/apigateway/controller/common/CommonController.java
@@ -3,6 +3,8 @@ package com.moneyfi.apigateway.controller.common;
 import com.moneyfi.apigateway.service.userservice.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -21,5 +23,14 @@ public class CommonController {
     @PostMapping("/logout")
     public ResponseEntity<Map<String, String>> logoutUser(@RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(userService.logout(token));
+    }
+
+    @Operation(summary = "Api to increase otp expiration time on user request")
+    @GetMapping("/extend-session")
+    public ResponseEntity<String> updateUserSessionExpirationTime(Authentication authentication,
+                                                                  @RequestHeader("Authorization") String authHeader,
+                                                                  @RequestParam long minutes) {
+        String username = ((UserDetails) authentication.getPrincipal()).getUsername();
+        return ResponseEntity.ok(userService.updateUserSessionExpirationTime(minutes, username, authHeader.substring(7)));
     }
 }
