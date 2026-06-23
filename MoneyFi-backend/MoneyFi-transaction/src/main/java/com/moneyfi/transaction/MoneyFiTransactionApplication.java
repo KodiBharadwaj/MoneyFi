df5fa468d227c9300e8933a2c2b95ce4dbcd2eac
Kodi Bharadwaj <138383233+KodiBharadwaj@users.noreply.github.com>
2026-06-23 16:48:56 +0530

    [Feature] Time zone added to services
    Co-authored-by: Bharadwaj Kodi <bharadwaj.kodi@usthealthproof.com>

--- MoneyFi-backend/MoneyFi-transaction/src/main/java/com/moneyfi/transaction/MoneyFiTransactionApplication.java
+++ MoneyFi-backend/MoneyFi-transaction/src/main/java/com/moneyfi/transaction/MoneyFiTransactionApplication.java
@@ -1,16 +1,32 @@
 package com.moneyfi.transaction;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
 @Slf4j
 public class MoneyFiTransactionApplication {
+
+    @Value("${application.time-zone}")
+    private String applicationTimezone;
+
+    @PostConstruct
+    private void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(applicationTimezone));
+        log.info("Application timezone is {}. Today = {} ", TimeZone.getDefault().toZoneId(), ZonedDateTime.now());
+        log.info("Application started as user: {}", System.getProperty("user.name"));
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyFiTransactionApplication.class, args);
 		log.info("Transaction Service Started");
--- MoneyFi-backend/Moneyfi-user/src/main/java/com/moneyfi/user/MoneyFiUserApplication.java
+++ MoneyFi-backend/Moneyfi-user/src/main/java/com/moneyfi/user/MoneyFiUserApplication.java
@@ -1,11 +1,16 @@
 package com.moneyfi.user;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +18,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @Slf4j
 public class MoneyFiUserApplication {
+
+    @Value("${application.time-zone}")
+    private String applicationTimezone;
+
+    @PostConstruct
+    private void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(applicationTimezone));
+        log.info("Application timezone is {}. Today = {} ", TimeZone.getDefault().toZoneId(), ZonedDateTime.now());
+        log.info("Application started as user: {}", System.getProperty("user.name"));
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(MoneyFiUserApplication.class, args);
 		log.info("User Service Started");
